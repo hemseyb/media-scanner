ed06b2d2cce61312dce0aee5b034a5f376c34712
hemseyb <127263807+hemseyb@users.noreply.github.com>
2025-03-29 23:40:54 -0700

    Initial commit of Flask app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+# Database connection
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname="media_db", user="postgres", password="1", host="localhost", port="5432"
+    )
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM public.media_data ORDER BY id ASC;")
+    rows = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', data=rows)
+
+if __name__ == '__main__':
+    app.run(debug=True)
